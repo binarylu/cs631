c6b3b15cb0dcd88c4bf5edd48356041d085ed3dc
Xiakun Lu <xlu9@stevens.edu>
2015-09-21 15:33:06 -0400

    submission

--- HW2/tcpm.c
+++ HW2/tcpm.c
@@ -1,6 +1,6 @@
 /*
  * "Advanced Programming in the UNIX Environment" Homework #2
- * Description: The tcp utility copies the contents of the source to target.
+ * Description: The tcpm utility copies the contents of the source to target.
  * Version 1.0
  * Date: 2015/09/21
  * Email: xlu9@stevens.edu
@@ -9,6 +9,7 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -27,10 +28,11 @@
  */
 static void getfilename(const char *path, const char **filename);
 
-/* BUFFSIZE is used to store the source file content */
-#ifndef BUFFSIZE
-#define BUFFSIZE 4096
-#endif
+#define HANDLE_ERROR(msg)        \
+do {                             \
+    perror(msg);                 \
+    exit(EXIT_FAILURE);          \
+} while (/* CONSTCOND */ 0)
 
 /*
  * Read the source file and copy its content to the target file. Source file can
@@ -40,11 +42,11 @@ int
 main(int argc, char *argv[])
 {
     int fdr, fdw, fddir;
-    int n;
-    char buf[BUFFSIZE];
+    int n, file_size;
 
     char *src = argv[1];
     char *dst = argv[2];
+    char *addr;
     struct stat st_src, st_dst;
 
     setprogname(argv[0]);
@@ -55,20 +57,16 @@ main(int argc, char *argv[])
     }
     
     /* Source file can not be a directory */
-    if (stat(src, &st_src) < 0) {
-        perror("Fail to access SOURCE");
-        exit(EXIT_FAILURE);
-    }
+    if (stat(src, &st_src) < 0)
+        HANDLE_ERROR("Fail to access SOURCE");
     if (S_ISDIR(st_src.st_mode)) {
         fprintf(stderr, "SOURCE cannot be a directory\n");
         exit(EXIT_FAILURE);
     }
 
     /* Open the source file as a regular file */
-    if ((fdr = open(src, O_RDONLY)) == -1) {
-        perror("Fail to read SOURCE");
-        exit(EXIT_FAILURE);
-    }
+    if ((fdr = open(src, O_RDONLY)) == -1)
+        HANDLE_ERROR("Fail to read SOURCE");
 
     /*
      * Create a new file with the specified name that user give if the target
@@ -109,26 +107,32 @@ main(int argc, char *argv[])
         }
     }
 
-    if (fstat(fdw, &st_dst) < 0) {
-        perror("Fail to access TARGET");
-        exit(EXIT_FAILURE);
-    }
+    if (fstat(fdw, &st_dst) < 0)
+        HANDLE_ERROR("Fail to access TARGET");
     /* Source file and target file can not be the same files. */
     if (st_src.st_ino == st_dst.st_ino) {
         fprintf(stderr, "'%s' and '%s' are the same file\n", src , dst);
     }
 
-    /* Read the source file contents and write to the target file. */
-    while ((n = read(fdr, buf, BUFFSIZE)) > 0) {
-        if (write(fdw, buf, n) != n) {
-            fprintf(stderr, "Copy failed!\n");
-            exit(EXIT_FAILURE);
+    /* map the source file content to the memory */
+    file_size = st_src.st_size;
+    addr = (char *)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fdr, 0);
+    if (addr == MAP_FAILED) {
+        close(fdr);
+        close(fdw);
+        HANDLE_ERROR("mmap error");
+    }
+    n = write(fdw, addr, file_size);
+    if (n != file_size) {
+        if (n == -1) {
+            HANDLE_ERROR("Write failed");
         }
+        fprintf(stderr, "partial write\n");
     }
-    if (n < 0) {
-        fprintf(stderr, "read error\n");
-        exit(EXIT_FAILURE);
-    }
+
+    /* unmap */
+    if (munmap(addr, file_size) == -1)
+        fprintf(stderr, "munmap failed\n");
 
     close(fdr);
     close(fdw);
